1632f9404d90929cf2f66507572aea40f6871a95
Levinh <23644831.vinh@student.iuh.edu.vn>
2025-12-10 15:57:23 +0700

    add embedding fearture

--- data_prepare.py
+++ data_prepare.py
@@ -1,4 +1,5 @@
 from datasets import *
+from datasets.clip_embeddings import maybe_extract_clip_embeddings
 from config import *
 from pytorch_lightning import seed_everything
 
@@ -7,7 +8,10 @@ def main(args):
     seed_everything(args.seed)
     dataset = dataset_factory(args)
     # Cần gọi load_dataset() hoặc preprocess() để trigger việc download
-    dataset.load_dataset()
+    data = dataset.load_dataset()
+    # Ngay sau khi dữ liệu (và ảnh) đã sẵn sàng, trích xuất CLIP embeddings
+    # cho image/text nếu được yêu cầu.
+    maybe_extract_clip_embeddings(dataset, data, args)
 
 if __name__ == "__main__":
     main(arg)
