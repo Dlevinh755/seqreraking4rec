a23e4982f29f3366403673db5f7ea2419edb4a27
Levinh <23644831.vinh@student.iuh.edu.vn>
2026-01-11 21:58:16 +0700

    add sport

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -2,12 +2,14 @@ from .ml_100k import ML100KDataset
 from .beauty import BeautyDataset
 from .games import GamesDataset
 from .clothes import ClothesDataset
+from .sports import SportsDataset
 
 DATASETS = {
     ML100KDataset.code(): ML100KDataset,
     BeautyDataset.code(): BeautyDataset,
     GamesDataset.code(): GamesDataset,
     ClothesDataset.code(): ClothesDataset,
+    SportsDataset.code(): SportsDataset,
 
 }
 
